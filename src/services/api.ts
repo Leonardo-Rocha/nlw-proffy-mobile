6fc4c48e135c6636daca320bf1eb64bfcadff70e
Leonardo Rocha <leonardocar.dm@gmail.com>
2020-08-08 20:45:44 -0400

    Separate API urls for production and development Add heroku API for production

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,11 @@
 import axios from 'axios';
 
+const baseURL = process.env.NODE_ENV === 'development'
+? 'http://192.168.2.102:3333'
+: 'https://nlw-proffy-api-rest.herokuapp.com/';
+
 const api = axios.create({
-  baseURL: 'http://192.168.2.102:3333',
+  baseURL,
 });
 
 export default api;
